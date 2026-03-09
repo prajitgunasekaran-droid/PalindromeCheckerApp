55a8e316c7c35cc8206f4cfd7335c6d5bb403ff4
prajitgunasekaran-droid <prajit.gunasekaran@gmail.com>
2026-03-09 09:46:44 +0530

    printing welcome message

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+/*
+ * Applicant entry point.
+ *
+ * This is the first method executed by the JVM
+ * when the program starts.
+ *
+ * @author Developer
+ * @Version 1.0
+ */
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized Successfully");
+
+
+    }
+
+}
