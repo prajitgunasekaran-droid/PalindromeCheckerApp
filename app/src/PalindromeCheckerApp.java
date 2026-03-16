0ba57b6af698737f2d011e56ba31833d10752e88
prajitgunasekaran-droid <prajit.gunasekaran@gmail.com>
2026-03-16 08:48:12 +0530

    Use Case 3: Centralized Room Inventory Management

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,34 +1,16 @@
-/*
- * Applicant entry point.
- *
- * This is the first method executed by the JVM
- * when the program starts.
- *
- * @author Developer
- * @Version 1.0
- */
-public class PalindromeCheckerApp {
-    /**
-     * Application entry point for UC2.
-     * @param args Command-line arguments
-     */
-
+class PalindromeCheck {
     public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
 
-        String input = "madam";
-        boolean isPalindrome = true;
-
-        // Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        for(int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
         }
 
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        if(str.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
-
